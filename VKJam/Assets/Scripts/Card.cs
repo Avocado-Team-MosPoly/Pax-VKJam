407001c27dd9d52956c55ebead939a0b5d5e802b
potikot <ildusxtr@mail.ru>
2023-03-12 20:50:39 +0300

    Fix: Monsters

--- VKJam/Assets/Scripts/Card.cs
+++ VKJam/Assets/Scripts/Card.cs
@@ -6,13 +6,16 @@ public class Card : MonoBehaviour
 {
     public List<GameObject> AllNormalCardPrefab;
     public List<GameObject> AllUebyCardPrefab;
+    public List<GameObject> AllNormalSprites;
+    public List<GameObject> AllUedySprites;
+
     public List<GameObject> Spawn;
     private List<GameObject> cardOnScene = new List<GameObject>();
     private List<string> activIngridientList;
     private GameObject activeCardPrefab;
     private List<Sprite> activeIngridients;
     private bool searchForCard;
-
+    
     public void Start()
     {
         NewCard();
@@ -23,17 +26,19 @@ public class Card : MonoBehaviour
         cardOnScene.Clear();
 
         int cardNumber = Random.Range(0, AllUebyCardPrefab.Count);
-        GameObject card = Instantiate(AllUebyCardPrefab[cardNumber], Spawn[2].transform.position, Spawn[2].transform.rotation, Spawn[2].transform);
-        card.GetComponent<CardInstance>().CardSpawner = this;
-        cardOnScene.Add(card);
+        CardInstance cardInstance = Instantiate(AllUebyCardPrefab[cardNumber], Spawn[2].transform.position, Spawn[2].transform.rotation, Spawn[2].transform).GetComponent<CardInstance>();
+        cardInstance.CardSpawner = this;
+        cardInstance.Monster = AllUedySprites[cardNumber];
+        cardOnScene.Add(cardInstance.gameObject);
         AllUebyCardPrefab.RemoveAt(cardNumber);
 
         for (int i = 0; i < 2; i++)
         {
             cardNumber = Random.Range(0, AllNormalCardPrefab.Count);
-            card = Instantiate(AllNormalCardPrefab[cardNumber], Spawn[i].transform.position, Spawn[i].transform.rotation, Spawn[i].transform);
-            card.GetComponent<CardInstance>().CardSpawner = this;
-            cardOnScene.Add(card);
+            cardInstance = Instantiate(AllNormalCardPrefab[cardNumber], Spawn[i].transform.position, Spawn[i].transform.rotation, Spawn[i].transform).GetComponent<CardInstance>();
+            cardInstance.CardSpawner = this;
+            cardInstance.Monster = AllNormalSprites[cardNumber];
+            cardOnScene.Add(cardInstance.gameObject);
             AllNormalCardPrefab.RemoveAt(cardNumber);
             searchForCard = true;
         }
