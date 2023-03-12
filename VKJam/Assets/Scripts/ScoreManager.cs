6a01a826c5d8192b63d8de9335952565181e6f11
Yargf0 <122349983+Yargf0@users.noreply.github.com>
2023-03-12 09:28:58 +0300

    Change score script

--- VKJam/Assets/Scripts/ScoreManager.cs
+++ VKJam/Assets/Scripts/ScoreManager.cs
@@ -1,11 +1,28 @@
 using UnityEngine;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
+    public TMP_Text Fishka;
+    private static TMP_Text fishka;
     public static int Score { get; private set; }
 
+    private void Start()
+    {
+        fishka = Fishka;
+    }
     public static void AddScore(int value)
     {
         Score += value;
+        fishka.text = Score.ToString();
+    }
+    public static void RemoveScore(int value)
+    {
+        Score -= value;
+        if (Score<0)
+        {
+            Score = 0;
+        }
+        fishka.text = Score.ToString();
     }
 }
