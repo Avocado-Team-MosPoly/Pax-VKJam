34d73dcd77d737b5aacc0d880896da8df17441a3
Рената Салихова <90962760+IRenataI@users.noreply.github.com>
2023-03-12 19:52:50 +0300

    MapC

---
+++ VKJam/Assets/Scripts/ShowRecepiesUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowRecepiesUI : MonoBehaviour
+{
+    public GameObject[] Recepies;
+
+    public void SpawnRecepies()
+    {
+        int cardNumber = Random.Range(0, Recepies.Length);
+        Recepies[cardNumber].SetActive(true);
+    }
+    public void Hide()
+    {
+        foreach (GameObject hide in Recepies)
+        {
+            hide.SetActive(false);
+        }
+    }
+
+}
