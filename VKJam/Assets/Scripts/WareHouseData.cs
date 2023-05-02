b3c5bf49ec3d88aa65583fc431464251cf74248e
Рената Салихова <90962760+IRenataI@users.noreply.github.com>
2023-05-02 18:51:04 +0300

    MenuShop

--- VKJam/Assets/Scripts/WareHouseData.cs
+++ VKJam/Assets/Scripts/WareHouseData.cs
@@ -3,18 +3,14 @@ public struct WareHouseData
 {
     public int productCode;
     public string productName;
-    public int productAmount;
-    public int workshopNumber;
-    public int productPricePerOneUnit;
+    public int productPrice;
     public UnityEngine.Sprite icon;
 
-    public WareHouseData(int productCode, string productName, int productAmount, int workshopNumber, int productPricePerOneUnit, UnityEngine.Sprite icon)
+    public WareHouseData(int productCode, string productName, int productPrice, UnityEngine.Sprite icon)
     {
         this.productCode = productCode;
         this.productName = productName;
-        this.productAmount = productAmount;
-        this.workshopNumber = workshopNumber;
-        this.productPricePerOneUnit = productPricePerOneUnit;
+        this.productPrice = productPrice;
         this.icon = icon;
     }
 
