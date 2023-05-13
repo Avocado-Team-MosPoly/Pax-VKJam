bdd020304f0631d5628061648d1f53191f2052bc
Рената Салихова <90962760+IRenataI@users.noreply.github.com>
2023-05-13 17:08:47 +0300

    122222

--- VKJam/Assets/Scripts/WareHouseData.cs
+++ VKJam/Assets/Scripts/WareHouseData.cs
@@ -5,13 +5,16 @@ public struct WareHouseData
     public string productName;
     public int productPrice;
     public UnityEngine.Sprite icon;
+    public bool IsDonateVault;
 
-    public WareHouseData(int productCode, string productName, int productPrice, UnityEngine.Sprite icon)
+    public WareHouseData(int productCode, string productName, int productPrice, UnityEngine.Sprite icon, bool IsDonateVault)
     {
         this.productCode = productCode;
         this.productName = productName;
         this.productPrice = productPrice;
         this.icon = icon;
+        this.IsDonateVault = IsDonateVault;
     }
+
 
 }
