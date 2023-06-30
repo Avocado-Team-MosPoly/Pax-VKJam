4809e35ac8d5b41be935f26453a891973c4305cb
Рената Салихова <r.r.salikhova@stud.mospolytech.ru>
2023-06-30 18:38:35 +0300

    Book an MENUTEST scene Changes

---
+++ VKJam/Assets/Scripts/RoomCreation.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using TMPro;
+
+
+public class RoomCreation : MonoBehaviour
+{
+    private int selectedPlayerCount = 0;
+    private bool isTeamMode = false;
+    private bool isRecipeModeStandart = false;
+    [SerializeField] private TMP_Dropdown cardsDropdown;
+    [SerializeField] private TMP_Dropdown roundDropdown;
+
+    public void SelectPlayerCount(int count)
+    {
+        selectedPlayerCount = count;
+    }
+
+    public void SelectGameMode(bool isTeamMode)
+    {
+        isTeamMode = isTeamMode;
+    }
+
+    public void SelectRecipeMode(bool isStandart)
+    {
+        isRecipeModeStandart = isStandart;
+    }
+
+    public void SelectDeck()
+    {
+        int selectedDeckIndex = cardsDropdown.value;
+        string selectedDeckName = cardsDropdown.options[selectedDeckIndex].text;
+    }
+
+    public void SelectRoundCount()
+    {
+        int selectedRoundIndex = roundDropdown.value;
+        int selectedRoundCount = int.Parse(roundDropdown.options[selectedRoundIndex].text);
+    }
+}
