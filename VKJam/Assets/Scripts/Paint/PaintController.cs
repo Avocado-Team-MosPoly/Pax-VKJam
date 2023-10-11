5053cc12ecfa4d92b9aa5d41463ba593574ccfbe
potikot <91075702+potikot@users.noreply.github.com>
2023-10-11 22:15:28 +0300

    Update

--- VKJam/Assets/Scripts/Paint/PaintController.cs
+++ VKJam/Assets/Scripts/Paint/PaintController.cs
@@ -1,11 +1,17 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PaintController : MonoBehaviour
 {
     [SerializeField] private Paint paint;
 
     [SerializeField, Range(0, 15)] private int scrollRate = 2;
+
+    [SerializeField] private TextMeshProUGUI sizeInfo;
+    [SerializeField] private Slider sizeSlider;
+
     private byte maxBrushSize = 16;
     private byte minBrushSize = 4;
     private int localBrushSize;
@@ -16,6 +22,16 @@ public class PaintController : MonoBehaviour
     private void Start()
     {
         localBrushSize = paint.BrushSize;
+        
+        sizeSlider.minValue = minBrushSize;
+        sizeSlider.maxValue = maxBrushSize;
+        sizeSlider.value = localBrushSize;
+
+        sizeSlider.wholeNumbers = true;
+
+        sizeSlider.onValueChanged.AddListener(ChangeBrushSize);
+
+        sizeInfo.text = localBrushSize.ToString();
     }
 
     private void Update()
@@ -27,7 +43,7 @@ public class PaintController : MonoBehaviour
     {
         int scrollDelta = (int)Input.mouseScrollDelta.y;
         
-        if (scrollDelta != 0f)
+        if (scrollDelta != 0)
         {
             localBrushSize += scrollRate * scrollDelta;
         }
@@ -38,7 +54,41 @@ public class PaintController : MonoBehaviour
             {
                 if (canSet)
                 {
+                    paint.ChangeSize((byte)localBrushSize);
+                    canSet = false;
+                }
+                else if (canSetCoroutine == null)
+                {
+                    canSetCoroutine = StartCoroutine(ScrollLock());
+                }
+            }
+        }
+    }
 
+    public void ChangeBrushSize(float value)
+    {
+        sizeInfo.text = value.ToString();
+
+        int scrollDelta = (int)value - paint.BrushSize;
+
+        if (scrollDelta != 0)
+        {
+            scrollDelta = Mathf.Clamp(scrollDelta - scrollDelta % 2, minBrushSize, maxBrushSize);
+            localBrushSize += scrollDelta;
+            
+            if (canSetCoroutine != null)
+            {
+                StopCoroutine(canSetCoroutine);
+                canSetCoroutine = null;
+            }
+        }
+        else if (localBrushSize != paint.BrushSize)
+        {
+            localBrushSize = Mathf.Clamp(localBrushSize, minBrushSize, maxBrushSize);
+            if (localBrushSize != paint.BrushSize)
+            {
+                if (canSet)
+                {
                     paint.ChangeSize((byte)localBrushSize);
                     canSet = false;
                 }
