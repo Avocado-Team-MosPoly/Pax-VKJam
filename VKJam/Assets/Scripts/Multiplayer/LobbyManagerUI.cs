bf4e76c45ac683e23782d3349a5ea1c6341e2d05
Yargf0 <122349983+Yargf0@users.noreply.github.com>
2023-11-14 23:00:21 +0300

    Change lobby3d

--- VKJam/Assets/Scripts/Multiplayer/LobbyManagerUI.cs
+++ VKJam/Assets/Scripts/Multiplayer/LobbyManagerUI.cs
@@ -1,8 +1,11 @@
 using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.Services.Authentication;
 using Unity.Services.Lobbies.Models;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class LobbyManagerUI : MonoBehaviour
 {
@@ -15,16 +18,22 @@ public class LobbyManagerUI : MonoBehaviour
 
     [SerializeField] private RectTransform lobbyListContainer;
     [SerializeField] private GameObject lobbyInfoTemplate;
-    [SerializeField] private List<GameObject> playerList;
+    [SerializeField] private GameObject ready;
+    [SerializeField] private List<GameObject> playerGameObjectList;
+    [SerializeField] private List<GameObject> playerReady;
 
     [SerializeField] private RectTransform playerListContainer;
     [SerializeField] private GameObject playerInfoPrefab;
+    [SerializeField] private List<bool> allPlayerReady;
+    private List<Player> players;
+
 
     private void Start()
     {
         createLobbyButton?.onClick.AddListener(LobbyManager.Instance.CreateLobby);
         leaveLobbyButton?.onClick.AddListener(LeaveLobby);
         updatePlayerList?.onClick.AddListener(LobbyManager.Instance.ListPlayers);
+        ready?.GetComponent<Button>().onClick.AddListener(ChangeReady);
 
         if (startGameButton)
         {
@@ -64,17 +73,72 @@ public class LobbyManagerUI : MonoBehaviour
 
     private void UpdatePlayerList(List<Player> players)
     {
-        foreach (GameObject player in playerList)
+        UpdatePlayerListServerRpc(players);
+    }
+    private int GetplayerNumber()
+    {
+        int i = 0;
+        for (; i < players.Count; i++)
+        {
+            if (players[i].Id == AuthenticationService.Instance.PlayerId)
+            {
+                
+                break;
+            }
+        }
+        return i;
+    }
+    public void ChangeReady()
+    {
+        UpdatePlayerReadyServerRpc(GetplayerNumber());
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void UpdatePlayerReadyServerRpc(int player)
+    {
+        allPlayerReady[player]=!allPlayerReady[player];
+        UpdatePlayerReadyClientRpc(allPlayerReady);
+    }
+    [ClientRpc]
+    private void UpdatePlayerReadyClientRpc(List<bool> players)
+    {
+        allPlayerReady = players;
+        for (int i = 0; i < playerReady.Count; i++)
+        {
+            playerReady[i].SetActive(allPlayerReady[i]);
+        }
+        if(NetworkManager.Singleton.IsServer)
+        {
+            bool allReady=true;
+            foreach (bool ready in allPlayerReady)
+            {
+                if (!ready)
+                    allReady = false;
+            }
+            if (allReady)
+            {
+                
+            }
+        }
+    }
+    [ServerRpc(RequireOwnership = false)]
+    private void UpdatePlayerListServerRpc(List<Player> players)
+    {
+        UpdatePlayerListClientRpc(players);
+    }
+    [ClientRpc]
+    private void UpdatePlayerListClientRpc(List<Player> players)
+    {
+        this.players=players;
+        foreach (GameObject player in playerGameObjectList)
         {
             player.SetActive(false);
         }
-        for (int i =0; i< players.Count; i++)
+        for (int i = 0; i < players.Count; i++)
         {
-            playerList[i].SetActive(true);
-
+            playerGameObjectList[i].SetActive(true);
         }
     }
-
     private void LeaveLobby()
     {
         LobbyManager.Instance.LeaveLobby();
