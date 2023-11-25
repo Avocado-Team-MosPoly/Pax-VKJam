257f502843e1d30bae1093f79198894ae744a9bc
Yargf0 <122349983+Yargf0@users.noreply.github.com>
2023-11-25 14:27:26 +0300

    ChangeLobbyTemplate

--- VKJam/Assets/Scripts/GetLobbyCodeAndConnect.cs
+++ VKJam/Assets/Scripts/GetLobbyCodeAndConnect.cs
@@ -1,19 +1,24 @@
+using Mono.Cecil.Cil;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class GetLobbyCodeAndConnect : MonoBehaviour
 {
     private string connectedLobbyCode;
+    public string LobbyCode;
     [SerializeField] private Button connect;
+    [SerializeField] private TMP_InputField code;
     void Awake()
     {
         connect.onClick.AddListener(() => LobbyManager.Instance.JoinLobby(connectedLobbyCode));
+        connect.onClick.AddListener(() => LobbyManager.Instance.JoinLobby(LobbyCode));
+        code.onValueChanged.AddListener(ChangeConnectedLobbyCode);
     }
     public void ChangeConnectedLobbyCode(string value)
     {
         connectedLobbyCode = value;
+        LobbyCode = code.text;
     }
-
 }
--- VKJam/Assets/Scripts/Multiplayer/LobbyInfoUI.cs
+++ VKJam/Assets/Scripts/Multiplayer/LobbyInfoUI.cs
@@ -8,13 +8,24 @@ public class LobbyInfoUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI lobbyName;
     [SerializeField] private TextMeshProUGUI playersCount;
     [SerializeField] private Button connectButton;
+    [SerializeField] private GameObject RegimeMode;
+    [SerializeField] private Sprite competitive;
+    [SerializeField] private Sprite comand;
 
     private Lobby lobby;
 
     public void SetLobby(Lobby lobby)
     {
         this.lobby = lobby;
-        
+        if (lobby.Data["IsTeamMode"].Value=="1")
+        {
+            RegimeMode.GetComponent<Image>().sprite = competitive;
+        }
+        else
+        {
+            RegimeMode.GetComponent<Image>().sprite = comand;
+        }
+
         lobbyName.text = lobby.Name;
         playersCount.text = lobby.Players.Count + "/" + lobby.MaxPlayers;
         connectButton.onClick.AddListener(() => {
