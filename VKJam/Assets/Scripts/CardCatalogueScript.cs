b6983f0a22d0421cf88ec55930aad35f9539205f
Tiractor <90830536+Tiractor@users.noreply.github.com>
2023-11-30 01:25:54 +0300

    CraftCardUpdate
    PHP script on server not ready at current moment

--- VKJam/Assets/Scripts/CardCatalogueScript.cs
+++ VKJam/Assets/Scripts/CardCatalogueScript.cs
@@ -6,13 +6,14 @@ using System;
 
 public class CardCatalogueScript : MonoBehaviour
 {
-    [HideInInspector] public List<CardSO> Monsters = new();
+    [HideInInspector] public List<CardSystem> Monsters = new();
 
     [SerializeField] private Image[] imageHolder = new Image[_cardInPage];
     [SerializeField] private TextMeshProUGUI[] nameHolder = new TextMeshProUGUI[_cardInPage];
 
     [SerializeField] private Button previousMonsterButton;
     [SerializeField] private Button nextMonsterButton;
+    [SerializeField] private GameObject BuyCanvas;
 
     //[SerializeField] private GameObject templateCanvas;
     [SerializeField] private PackCardSO packCardSO;
@@ -23,6 +24,8 @@ public class CardCatalogueScript : MonoBehaviour
     private int _currentPage;
     private const int _cardInPage = 8;
 
+    private int _bufferIdCard;
+
     private void Start()
     {
         TakePack();
@@ -32,23 +35,33 @@ public class CardCatalogueScript : MonoBehaviour
         Initialize();
     }
 
+    public void SetBuffer(int cardDBIndex)
+    {
+        _bufferIdCard = cardDBIndex;
+    }
+
     public void TakePack()
     {
         Monsters.Clear();
 
         for (int i = 0; i < packCardSO.CardInPack.Length; i++)
         {
-            if (packCardSO.CardInPack[i].CardIsInOwn == true)
-            {
+            //if (packCardSO.CardInPack[i].CardIsInOwn == true)
+            
                 print(packCardSO.CardInPack[i].Card.Id);
-                Monsters.Add(packCardSO.CardInPack[i].Card);
-            }
+                Monsters.Add(packCardSO.CardInPack[i]);
+            
         }
     }
 
     public void SetPack(PackCardSO _packCardSO)
     {
         packCardSO = _packCardSO;
+    }
+    public void BuyCard(bool ForThePieces)
+    {
+        Debug.Log("Trying buy card by id - " + _bufferIdCard);
+        Php_Connect.Request_CraftCardTry(_bufferIdCard, ForThePieces);
     }
     private void Initialize()
     {
@@ -95,10 +108,16 @@ public class CardCatalogueScript : MonoBehaviour
 
             if (index < Monsters.Count)
             {
-                imageHolder[i].GetComponentInChildren<Button>().gameObject.SetActive(true);
+                Button temp = imageHolder[i].GetComponentInChildren<Button>();
+                temp.gameObject.SetActive(!Monsters[index].CardIsInOwn);
+                temp.onClick.RemoveAllListeners();
+                temp.onClick.AddListener(() => 
+                SetBuffer(Monsters[index].CardDBIndex));
+                temp.onClick.AddListener(() =>
+                BuyCanvas.SetActive(true));
                 imageHolder[i].color = _normalColor;
-                imageHolder[i].sprite = Sprite.Create(Monsters[index].CardTexture, new Rect(0.0f, 0.0f, Monsters[index].CardTexture.width, Monsters[index].CardTexture.height), new Vector2(0.5f, 0.5f), 100.0f);
-                nameHolder[i].text = Monsters[index].Id;
+                imageHolder[i].sprite = Sprite.Create(Monsters[index].Card.CardTexture, new Rect(0.0f, 0.0f, Monsters[index].Card.CardTexture.width, Monsters[index].Card.CardTexture.height), new Vector2(0.5f, 0.5f), 100.0f);
+                nameHolder[i].text = Monsters[index].Card.Id;
             }
             else
             {
